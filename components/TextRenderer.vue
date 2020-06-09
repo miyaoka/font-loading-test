2f2cb21d169819a141b7a0d3b7de818c8d76ae8d
Masaya Kazama <miyan@t-p.jp>
2020-06-09 23:05:47 +0900

    WIP

---
+++ components/TextRenderer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :style="style" class="text"><slot /></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    family: {
+      type: String,
+      default: ''
+    },
+    weight: {
+      type: String,
+      default: '400'
+    }
+  },
+  computed: {
+    style(): Record<string, string> {
+      return {
+        fontFamily: `'${this.family}'`,
+        fontWeight: this.weight
+      }
+    }
+  }
+})
+</script>
+
+<style scoped>
+.text {
+  border: 1px solid #999;
+  padding: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,182 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        font-loading-test
-      </h1>
-      <h2 class="subtitle">
-        My stunning Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <client-only>
+      <i class="fa fa-user-circle" aria-hidden="true"></i>
+
+      <TextRenderer
+        v-for="(font, i) in useFontList"
+        :key="i"
+        :family="font.family"
+        :weight="font.weight"
+      >
+        abcdefgHIJKLMN0123456789
+        この書生の掌の裏うちでしばらくはよい心持に坐っておったが、しばらくすると非常な速力で運転し始めた。
+      </TextRenderer>
+    </client-only>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import TextRenderer from '@/components/TextRenderer.vue'
+// import webfonts from '@/assets/webfonts.json'
 
+// const googleFontLink = ()
+function getFontParam(family: string, variants: string[]): string {
+  return `${family}:${variants
+    .map((variant) => {
+      return variant
+        .replace(/italic/, 'i')
+        .replace(/regular/, '400')
+        .replace(/^i$/, '400i')
+    })
+    .join(',')}`
+}
 export default Vue.extend({
   components: {
-    Logo
+    TextRenderer
+  },
+  data() {
+    return {
+      useFontList: [
+        {
+          family: 'Montserrat',
+          weight: '200'
+        },
+        {
+          family: 'Metal Mania',
+          weight: '400'
+        },
+        // {
+        //   family: 'Kosugi',
+        //   weight: '400'
+        // },
+        {
+          family: 'Roboto',
+          weight: '900i'
+        },
+        {
+          family: 'Noto Sans JP',
+          weight: '100'
+        },
+        {
+          family: 'Noto Sans JP',
+          weight: '300'
+        },
+        {
+          family: 'Noto Sans JP',
+          weight: '400'
+        },
+        {
+          family: 'Noto Sans JP',
+          weight: '500'
+        },
+        {
+          family: 'Noto Sans JP',
+          weight: '700'
+        },
+        {
+          family: 'Noto Sans JP',
+          weight: '900'
+        },
+        {
+          family: 'Noto Serif JP',
+          weight: '100'
+        },
+        {
+          family: 'Noto Serif JP',
+          weight: '300'
+        },
+        {
+          family: 'Noto Serif JP',
+          weight: '400'
+        },
+        {
+          family: 'Noto Serif JP',
+          weight: '500'
+        },
+        {
+          family: 'Noto Serif JP',
+          weight: '700'
+        },
+        {
+          family: 'Noto Serif JP',
+          weight: '900'
+        }
+      ],
+      useOnly: true
+    }
+  },
+  computed: {
+    fontParams(): string[] {
+      if (this.useOnly) {
+        return this.useFontList.map((font) => {
+          return getFontParam(
+            font.family,
+            [font.weight]
+            //  ['100,300,400,500,700,900']
+          ) //, )
+        })
+      }
+      return []
+
+      // return webfonts.items
+      //   .filter((item) => item.subsets.includes('japanese'))
+      //   .map((item) => {
+      //     const { family, variants } = item
+      //     return getFontParam(family, variants)
+      //   })
+    },
+    fontLinkList(): string[] {
+      // return this.fontParams
+
+      return this.fontParams.reduce((acc: string[], param) => {
+        if (acc.length === 0) {
+          acc.push(param)
+        } else {
+          const last = acc[acc.length - 1] || ''
+          if (last.length > 5000) {
+            acc.push(param)
+          } else {
+            acc[acc.length - 1] += `|${param}`
+          }
+        }
+        return acc
+      }, [])
+    }
+  },
+  head(): {} {
+    console.log(this.fontLinkList)
+
+    return {
+      title: 'title',
+      link: [
+        ...this.fontLinkList.map((item) => {
+          return {
+            hid: 'googleFonts',
+            rel: 'preload',
+            onload: "this.onload=null;this.rel='stylesheet'",
+            // rel: 'stylesheet',
+            as: 'style',
+            href: `https://fonts.googleapis.com/css?family=${item}&display=swap`
+          }
+        }),
+
+        {
+          hid: 'fa',
+          rel: 'preload',
+          as: 'style',
+          href: 'https://studio.design/css/font-awesome.min.css',
+          onload: "this.onload=null;this.rel='stylesheet'"
+        }
+      ]
+    }
   }
 })
 </script>
 
-<style>
+<style scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  font-size: 30px;
 }
 </style>
